8b41309bafd27023266245919b7896fe888a1def
joelcolucci <joelcolucci@gmail.com>
2018-09-25 12:15:10 -0400

    refactor: improve resolver merge setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 const { GraphQLServer } = require('graphql-yoga');
 const mongoose = require('mongoose');
 
-const Query = require('./resolvers/Query/index');
-const Mutation = require('./resolvers/Mutation/index');
+const resolvers = require('./resolvers/index');
+
 
 const MONGO_DB_URL = 'mongodb://localhost:27017/graphql';
 const DEVELOPMENT_SERVER_URL = 'http://localhost:4000';
-
-
-const resolvers = {
-  Query,
-  Mutation
-};
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
@@ -20,7 +14,6 @@ const server = new GraphQLServer({
 
 
 mongoose.connect(MONGO_DB_URL, {useNewUrlParser: true});
-
 server.start(() => {
   console.log(`Server is running on ${DEVELOPMENT_SERVER_URL}`);
 });
---
+++ src/resolvers/index.js
@@ -0,0 +1,11 @@
+const Query = require('./Query/index');
+const Mutation = require('./Mutation/index');
+
+
+let resolvers = {
+  Query,
+  Mutation
+};
+
+
+module.exports = resolvers;
